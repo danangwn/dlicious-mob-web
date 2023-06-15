a89e650708e105c4c9978a660b837c76520482e4
Danang Wahyu Nugraha <danangwahyu.nugraha@gmail.com>
2023-06-15 13:37:32 +0700

    Add files via upload
    initial commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="container">
+        <div class="hero-text">
+            <div class="main-hero-text"></div>
+            <p>
+                <strong>Everyone</strong> Deserves Special
+                <br>
+                <strong>Cakes</strong> and <strong>Cookies</strong>
+            </p>
+            <div class="hero-subtext">
+                <p>
+                    We're home-based custom cake shop at Sawangan, Depok.
+                    <br>
+                    Our missions is to delivered love and happiness trough the
+                    <br>
+                    sweetness and beauty of a cake
+                </p>
+            </div>
+            <div class="hero-button">
+                <div class="row">
+                    <div class="col">
+                        <button class="btn btn-primary" style="margin-right: 12px;">
+                            <strong>Order Now</strong>
+                        </button>
+                        <a class="see-catalogs" href="#">
+                            See Our Catalogs
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Navbar from '../components/NavBar.vue'
+import Hero from '../components/Hero.vue'
+</script>
+
+<template>
+  <main>
+    <header>
+      <Navbar />
+    </header>
+
+    <body>
+        <div class="container">
+        <Hero />
+      </div>
+    </body>
+  </main>
+</template>
